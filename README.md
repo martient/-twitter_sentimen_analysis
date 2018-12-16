d64d82d1467fc540ad9b1ec995195ecc6ce86ded
Arnaud <arnaud.leherpeur@epitech.eu>
2018-12-16 08:45:29 +0000

    Setup cursor for analyzing

--- README.md
+++ README.md
@@ -18,3 +18,43 @@ CONSUMER_SECRET = ""
 ```
 pip install tweepy
 ```
+
+### Lib pandas
+
+```
+pip install pandas
+```
+
+### Lib numpy
+
+```
+pip install numpy
+```
+
+### Lib matplotlib
+
+```
+pip install numpy
+```
+
+
+### Lib textblob
+
+```
+pip install textblob
+```
+
+> https://youtu.be/1gQ6uG5Ujiw?t=2609
+
+
+``` python
+print(np.mean(data_frame['length']))
+
+    print(np.max(data_frame['likes']))
+    print(np.max(data_frame['retweets']))
+    time_likes = pd.Series(data=data_frame['likes'].values, index=data_frame['date'])
+    time_likes.plot(figsize=(16, 4), label="likes", legend=True)
+    time_retweets = pd.Series(data=data_frame['retweets'].values, index=data_frame['date'])
+    time_retweets.plot(figsize=(16, 4), label="retweets", legend=True)
+    plt.show()
+```
--- twitter_stream.py
+++ twitter_stream.py
@@ -1,20 +1,64 @@
+from tweepy import API
+from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from textblob import TextBlob
 
 import twitter_credentials
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
 
-class TwitterStreamer():
-    def stream_tweets(self, fetched_tweets_filename, hash_tag_list ):
-        listener = StdOutListener(fetched_tweets_filename)
+
+class TwitterClient():
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().autenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+        self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+    
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+    
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+class TwitterAuthenticator():
+    def autenticate_twitter_app(self):
         auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
         auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
+
+class TwitterStreamer():
+    def __init__(self):
+        self.twitter_authenticator = TwitterAuthenticator()
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list ):
+        listener = TwitterListener(fetched_tweets_filename)
+        auth = self.twitter_authenticator.autenticate_twitter_app()
         stream = Stream(auth, listener)
         stream.filter(track=hash_tag_list)
 
-class StdOutListener(StreamListener):
+class TwitterListener(StreamListener):
     def __init__(self, fetched_tweets_filename):
         self.fetched_tweets_filename = fetched_tweets_filename
+    
     def on_data(self, data):
         try:
             print(data)
@@ -26,11 +70,48 @@ class StdOutListener(StreamListener):
         return True
 
     def on_error(self, status):
+        if status == 420:
+            return False
         print(status)
 
-if __name__ == "__main__":
-    hash_tag_list = ["donald trump"]
-    fetched_tweets_filename = "tweets.json"
+class TweetAnalyser():
+    def tweets_to_data_frame(self, tweets):
+        data_frame = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
+        data_frame['id'] = np.array([tweet.id for tweet in tweets])
+        data_frame['length'] = np.array([len(tweet.text) for tweet in tweets])
+        data_frame['date'] = np.array([tweet.created_at for tweet in tweets])
+        data_frame['source'] = np.array([tweet.source for tweet in tweets])
+        data_frame['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        data_frame['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        return data_frame
 
-    twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def analyze_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
+
+if __name__ == "__main__":
+    twitter_client = TwitterClient()
+    tweet_analyser = TweetAnalyser()
+    api = twitter_client.get_twitter_client_api()
+    tweets = api.user_timeline(screen_name="realDonaldTrump")
+    data_frame = tweet_analyser.tweets_to_data_frame(tweets)
+    data_frame['sentiment'] = np.array([tweet_analyser.analyze_sentiment(tweet) for tweet in data_frame['tweets']])
+
+    time_likes = pd.Series(data=data_frame['likes'].values, index=data_frame['date'])
+    time_likes.plot(figsize=(16, 4), label="likes", legend=True)
+    time_retweets = pd.Series(data=data_frame['retweets'].values, index=data_frame['date'])
+    time_retweets.plot(figsize=(16, 4), label="retweets", legend=True)
+    plt.show()    
+    time_retweets = pd.Series(data=data_frame['sentiment'].values, index=data_frame['date'])
+    time_retweets.plot(figsize=(16, 4), label="sentiment", legend=True)
+    plt.show()    
+    print(data_frame.head)
